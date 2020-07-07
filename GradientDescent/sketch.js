e6747443a0c78c709a1057a388e127c2a074f9a6
Vamsi995 <saivamsi.ds123@gmail.com>
2020-07-07 14:16:22 +0530

    Added the grafica library for 2d plots

--- GradientDescent/sketch.js
+++ GradientDescent/sketch.js
@@ -4,16 +4,22 @@ function setup(){
 
     createCanvas(600,600)
 
+    plot = new GPlot(new p5());
+		plot.setPos(25, 25);
+		plot.setDim(300, 300);
 }
-
 
 
 function draw(){
 
     background(255);
 
-    
-
+	plot.beginDraw();
+		plot.drawBackground();
+		plot.drawBox();
+		plot.drawXAxis();
+		plot.drawYAxis();
+		plot.drawTopAxis();
 
 
    
